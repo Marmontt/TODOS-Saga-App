e7042191b7893ce2fddae26f6bc089ceb2591ccc
Denis Gluscenco <dgluscenco@endava.com>
2019-09-19 17:09:18 +0300

    fixed package.json

--- src/state/todos/reducer.js
+++ src/state/todos/reducer.js
@@ -2,12 +2,12 @@ import * as types from './types';
 
 const initialState = {
     todos: []
-}
+};
 const todosReducer = (state = initialState, action) => {
     switch (action.type) {
         default:
             return state;
     }
-}
+};
 
 export default todosReducer;
