acc85a694d1a3eb7e600bf9f63a449630b8a9058
Denis Gluscenco <dgluscenco@endava.com>
2019-09-19 16:58:24 +0300

    basic redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import store from './state/store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
---
+++ src/state/todos/types.js
@@ -0,0 +1,4 @@
+export const ADD_TODO = 'TODO/ADD';
+export const REMOVE_TODO = 'TODO/REMOVE';
+export const UPDATE_TODO = 'TODO/UPDATE';
+export const GET_TODOS = 'TODO/GET';
