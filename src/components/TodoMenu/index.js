98952eb0efaedb4b2cae3e2eef6fcefef6af5199
Denis Gluscenco <dgluscenco@endava.com>
2019-09-20 15:08:51 +0300

    Added layout

--- src/components/TodoMenu/index.js
+++ src/components/TodoMenu/index.js
@@ -1,15 +1,9 @@
 import React, {useState} from 'react';
-import {connect} from 'react-redux'
-
-import * as todoActions from '../../state/todos/action'
 import TodoColorBox from "./components/TodoColorBox";
-
 import './index.css';
 
-const TodoMenu = (props) => {
+const TodoMenu = () => {
     const [todoText, setTodoText] = useState('');
-    const [chosenColor, setColor] = useState('#eb716a');
-
     return (
         <div className={'todo-menu-container'}>
             <input
@@ -19,21 +13,15 @@ const TodoMenu = (props) => {
                 value={todoText}
                 onChange={e => setTodoText(e.target.value)}
             />
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#eb716a'}/>
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#fe74a2'}/>
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#9170cc'}/>
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#5cb3f9'}/>
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#6ed3e5'}/>
-            <TodoColorBox chosenColor={chosenColor} setColor={setColor} color={'#ffe07f'}/>
-            <button onClick={() => props.addTodo(todoText, chosenColor)} className={'todo-menu-button'}>Add</button>
+            <TodoColorBox color={'#eb716a'}/>
+            <TodoColorBox color={'#fe74a2'}/>
+            <TodoColorBox color={'#9170cc'}/>
+            <TodoColorBox color={'#5cb3f9'}/>
+            <TodoColorBox color={'#6ed3e5'}/>
+            <TodoColorBox color={'#ffe07f'}/>
+            <button className={'todo-menu-button'}>Add</button>
         </div>
     )
 };
 
-
-const mapDispatchToProps = {
-    addTodo: todoActions.addTodo
-};
-
-
-export default connect(null, mapDispatchToProps)(TodoMenu);
+export default TodoMenu;
