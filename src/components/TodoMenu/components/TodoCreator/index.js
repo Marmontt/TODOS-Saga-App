27e6ba61d058c7f88f2ea2d62c89798afed26605
Denis Gluscenco <dgluscenco@endava.com>
2019-09-23 17:18:46 +0300

    Great commit!

--- src/components/TodoMenu/components/TodoCreator/index.js
+++ src/components/TodoMenu/components/TodoCreator/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
+import * as todoActions from "../../../../state/todos/action";
+import {connect} from 'react-redux';
 
-const TodoCreator = ({color, text}) => {
+const TodoCreator = ({color, text, fulfilled, removeTodo, todoIndex}) => {
+    const [todoFulfilled, setTodoState] = useState(fulfilled);
+
     return (
         <div style={{background: color}} className={'todoCreator-container'}>
-            <input className={'todoCreator-checkbox'} type="checkbox"/>
+            <input onChange={() => {
+                console.log(fulfilled);
+                // setTodoState(true);
+                removeTodo(todoIndex);
+            }} className={'todoCreator-checkbox'} checked={todoFulfilled} type="checkbox"/>
             <div className={'todoCreator-text'}>{text}</div>
         </div>
     );
 };
 
-export default TodoCreator;
+const mapActionsToProps = {
+    removeTodo: todoActions.removeTodo
+};
+
+export default connect(null, mapActionsToProps)(TodoCreator);
