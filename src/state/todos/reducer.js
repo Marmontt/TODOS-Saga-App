ee7b97a20035aacb4f6335af9ee52741bf5811f7
Denis Gluscenco <dgluscenco@endava.com>
2019-09-23 17:18:46 +0300

    Great commit!

--- src/state/todos/reducer.js
+++ src/state/todos/reducer.js
@@ -1,10 +1,30 @@
 import * as types from './types';
 
 const initialState = {
+    chosenColor: '#eb716a',
     todos: []
 };
+
 const todosReducer = (state = initialState, action) => {
     switch (action.type) {
+        case types.ADD_TODO_SUCCESS:
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.payload.text,
+                        color: action.payload.color,
+                        fulfilled: action.payload.fulfilled
+                    }
+                ]
+            };
+        case types.REMOVE_TODO_SUCCESS:
+            let todos = state.todos.slice();
+            return {
+                ...state,
+                todos: todos.filter(todo => todo.fulfilled === false)
+            };
         default:
             return state;
     }
--- src/state/todos/types.js
+++ src/state/todos/types.js
@@ -2,3 +2,10 @@ export const ADD_TODO = 'TODO/ADD';
 export const REMOVE_TODO = 'TODO/REMOVE';
 export const UPDATE_TODO = 'TODO/UPDATE';
 export const GET_TODOS = 'TODO/GET';
+export const SET_COLOR = 'SET/COLOR';
+
+
+
+export const ADD_TODO_SUCCESS = 'TODO/ADD/SUCCESS';
+export const REMOVE_TODO_SUCCESS = 'TODO/REMOVE/SUCCESS';
+
